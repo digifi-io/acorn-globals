986c6d9f3098e63abecb336b42168ad2f4517f0e
Ilya Tishonok <ilya.tishonok@satelliteinnovations.io>
2021-08-04 12:57:51 +0300

    Re-implement globals detecting

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var acorn = require('acorn');
-var walk = require('acorn-walk');
+const acorn = require('acorn');
+const acornLoose = require('acorn-loose');
+const walk = require('acorn-walk');
 
 function isScope(node) {
   return node.type === 'FunctionExpression' || node.type === 'FunctionDeclaration' || node.type === 'ArrowFunctionExpression' || node.type === 'Program';
 }
+
 function isBlockScope(node) {
   return node.type === 'BlockStatement' || isScope(node);
 }
@@ -18,49 +20,72 @@ function declaresThis(node) {
   return node.type === 'FunctionExpression' || node.type === 'FunctionDeclaration';
 }
 
-function reallyParse(source, options) {
-  var parseOptions = Object.assign({}, options,
+function reallyParse(source, options, fallbackToLoose) {
+  const parseOptions = Object.assign({}, options,
     {
       allowReturnOutsideFunction: true,
       allowImportExportEverywhere: true,
-      allowHashBang: true
-    }
+      allowHashBang: true,
+      locations: true,
+    },
   );
-  return acorn.parse(source, parseOptions);
-}
-module.exports = findGlobals;
-module.exports.parse = reallyParse;
-function findGlobals(source, options) {
-  options = options || {};
-  var globals = [];
-  var ast;
-  // istanbul ignore else
-  if (typeof source === 'string') {
-    ast = reallyParse(source, options);
-  } else {
-    ast = source;
+
+  try {
+    const acornParseResult = acorn.parse(source, parseOptions);
+
+    return {
+      ast: acornParseResult,
+    };
+  } catch (err) {
+    if (fallbackToLoose) {
+      return {
+        ast: acornLoose.parse(source, parseOptions),
+        parsingError: err.message,
+      };
+    }
+
+    throw err;
   }
+}
+
+function parseWithGlobals(source, options, fallbackToLoose) {
+  options = options || {};
+
+  const globals = [];
+  let ast;
+
+  const {
+    ast,
+    parsingError,
+  } = (typeof source === 'string') ? reallyParse(source, options, fallbackToLoose) : { ast: source };
+
   // istanbul ignore if
   if (!(ast && typeof ast === 'object' && ast.type === 'Program')) {
     throw new TypeError('Source must be either a string of JavaScript or an acorn AST');
   }
-  var declareFunction = function (node) {
-    var fn = node;
+
+  const declareFunction = function (node) {
+    const fn = node;
+
     fn.locals = fn.locals || Object.create(null);
     node.params.forEach(function (node) {
       declarePattern(node, fn);
     });
+
     if (node.id) {
       fn.locals[node.id.name] = true;
     }
   };
-  var declareClass = function (node) {
+
+  const declareClass = function (node) {
     node.locals = node.locals || Object.create(null);
+
     if (node.id) {
       node.locals[node.id.name] = true;
     }
   };
-  var declarePattern = function (node, parent) {
+
+  const declarePattern = function (node, parent) {
     switch (node.type) {
       case 'Identifier':
         parent.locals[node.name] = true;
@@ -86,94 +111,130 @@ function findGlobals(source, options) {
         throw new Error('Unrecognized pattern type: ' + node.type);
     }
   };
-  var declareModuleSpecifier = function (node, parents) {
+
+  const declareModuleSpecifier = function (node) {
     ast.locals = ast.locals || Object.create(null);
     ast.locals[node.local.name] = true;
   };
+
   walk.ancestor(ast, {
     'VariableDeclaration': function (node, parents) {
-      var parent = null;
-      for (var i = parents.length - 1; i >= 0 && parent === null; i--) {
+      let parent = null;
+
+      for (let i = parents.length - 1; i >= 0 && parent === null; i--) {
         if (node.kind === 'var' ? isScope(parents[i]) : isBlockScope(parents[i])) {
           parent = parents[i];
         }
       }
+
       parent.locals = parent.locals || Object.create(null);
       node.declarations.forEach(function (declaration) {
         declarePattern(declaration.id, parent);
       });
     },
     'FunctionDeclaration': function (node, parents) {
-      var parent = null;
-      for (var i = parents.length - 2; i >= 0 && parent === null; i--) {
+      let parent = null;
+
+      for (let i = parents.length - 2; i >= 0 && parent === null; i--) {
         if (isScope(parents[i])) {
           parent = parents[i];
         }
       }
+
       parent.locals = parent.locals || Object.create(null);
+
       if (node.id) {
         parent.locals[node.id.name] = true;
       }
+
       declareFunction(node);
     },
     'Function': declareFunction,
     'ClassDeclaration': function (node, parents) {
-      var parent = null;
-      for (var i = parents.length - 2; i >= 0 && parent === null; i--) {
+      let parent = null;
+
+      for (let i = parents.length - 2; i >= 0 && parent === null; i--) {
         if (isBlockScope(parents[i])) {
           parent = parents[i];
         }
       }
+
       parent.locals = parent.locals || Object.create(null);
+
       if (node.id) {
         parent.locals[node.id.name] = true;
       }
+
       declareClass(node);
     },
     'Class': declareClass,
     'TryStatement': function (node) {
-      if (node.handler === null) return;
+      if (node.handler === null) {
+        return;
+      }
+
       node.handler.locals = node.handler.locals || Object.create(null);
+
       declarePattern(node.handler.param, node.handler);
     },
     'ImportDefaultSpecifier': declareModuleSpecifier,
     'ImportSpecifier': declareModuleSpecifier,
     'ImportNamespaceSpecifier': declareModuleSpecifier
   });
+
   function identifier(node, parents) {
-    var name = node.name;
-    if (name === 'undefined') return;
-    for (var i = 0; i < parents.length; i++) {
+    const name = node.name;
+
+    if (name === 'undefined') {
+      return;
+    }
+
+    for (let i = 0; i < parents.length; i++) {
       if (name === 'arguments' && declaresArguments(parents[i])) {
         return;
       }
+
       if (parents[i].locals && name in parents[i].locals) {
         return;
       }
     }
+
     node.parents = parents.slice();
     globals.push(node);
   }
+
   walk.ancestor(ast, {
     'VariablePattern': identifier,
     'Identifier': identifier,
     'ThisExpression': function (node, parents) {
-      for (var i = 0; i < parents.length; i++) {
+      for (let i = 0; i < parents.length; i++) {
         if (declaresThis(parents[i])) {
           return;
         }
       }
+
       node.parents = parents.slice();
       globals.push(node);
     }
   });
-  var groupedGlobals = Object.create(null);
+
+  const groupedGlobals = Object.create(null);
+
   globals.forEach(function (node) {
-    var name = node.type === 'ThisExpression' ? 'this' : node.name;
+    const name = node.type === 'ThisExpression' ? 'this' : node.name;
+
     groupedGlobals[name] = (groupedGlobals[name] || []);
     groupedGlobals[name].push(node);
   });
-  return Object.keys(groupedGlobals).sort().map(function (name) {
-    return {name: name, nodes: groupedGlobals[name]};
-  });
+  
+  return {
+    ast,
+    globals: Object.keys(groupedGlobals).sort().map((name) => ({
+      name, nodes: groupedGlobals[name],
+    })),
+    parsingError,
+  };
 }
+
+module.exports = parseWithGlobals;
+module.exports.parse = reallyParse;
